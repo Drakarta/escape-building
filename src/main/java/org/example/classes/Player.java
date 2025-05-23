5166604e76d0e130bb323e429a871f203270bbce
Mvd-student <24148695@student.hhs.nl>
2025-05-23 12:40:36 +0200

    changes login to use constructor
    added player constructor

--- src/main/java/org/example/classes/Player.java
+++ src/main/java/org/example/classes/Player.java
@@ -1,11 +1,23 @@
 package org.example.classes;
 
+import org.example.classes.rooms.Coordinates;
+import org.example.classes.rooms.cells.PlayerCell;
+
 public class Player {
     private int id;
     private String name;
     private String username;
     private int hp;
     private String currentRoom;
+    private PlayerCell playercell;
+
+    public Player(String name, String username, int hp, String currentRoom, PlayerCell playercell){
+        this.name = name;
+        this.username = username;
+        this.hp = hp;
+        this.currentRoom = currentRoom;
+        this.playercell = playercell;
+    }
 
 
     public void setId(int id) {this.id = id;}
