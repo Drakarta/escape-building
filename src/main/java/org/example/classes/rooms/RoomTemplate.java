f5933b48bab730bf51330d2852058dc08e7b9920
Ant <Drakarta3@gmail.com>
2025-05-23 11:43:33 +0200

    merge 1

--- src/main/java/org/example/classes/rooms/RoomTemplate.java
+++ src/main/java/org/example/classes/rooms/RoomTemplate.java
@@ -23,7 +23,7 @@ public abstract class RoomTemplate {
     public abstract void displayRoom();
     public abstract void question();
 
-    public Object getName() {
+    public String getName() {
         return name;
     }
     public String getDescription() {
@@ -38,46 +38,4 @@ public abstract class RoomTemplate {
     public RoomLayout getRoomLayout() {
         return roomLayout;
     }
-
-//     public final void play(int roomIndex) {
-//         RoomList rooms = RoomList.getInstance();
-//         PlayerCell player = new PlayerCell(new Coordinates(1, 1));
-//         RoomLayout layout = getRoomLayout();
-//         PlayerMovement movement = new PlayerMovement(player, layout);
-
-//         Scanner scanner = new Scanner(System.in);
-//         layout.printRoomLayout(player);
-
-//         System.out.print("Enter command (WASD to move, E to interact, Q to quit): ");
-
-//         while (true) {
-//             System.out.println("Player Y: " + player.getCoordinates().getY());
-//             System.out.println("Room height - 1: " + (layout.getRoomLayout().size() - 1));
-
-//             String input = scanner.nextLine().toLowerCase();
-//             if (input.isEmpty()) continue;
-
-//             movement.handleInput(input);
-// //            layout.clearScreen();
-//             layout.printRoomLayout(player);
-            
-//             if (player.getCoordinates().getY() == 0) {
-//                 roomIndex++;
-// //                layout.clearScreen();
-//                 rooms.getRoomList().get(roomIndex).play(roomIndex);
-//             }
-            
-//             if (player.getCoordinates().getY() == layout.getRoomLayout().size() - 1) {
-//                 System.out.println("Test");
-//                 if (roomIndex > 0) {
-//                     System.out.println("test2");
-//                     roomIndex--;
-//                     layout.clearScreen();
-//                     rooms.getRoomList().get(roomIndex).play(roomIndex);
-//                 } else {
-//                     System.out.println("This is the first room");
-//                 }
-//             }
-//         }
-    // }
 }
