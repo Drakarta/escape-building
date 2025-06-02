23e895ffa39dc3b10077eb7c196a10b3c2fa3504
Anthony <drakarta3@gmail.com>
2025-06-02 18:59:48 +0200

    jpa works

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,8 +1,18 @@
 package org.example;
 
+import org.example.classes.Player;
+import org.example.classes.rooms.Coordinates;
+import org.example.classes.rooms.cells.PlayerCell;
+import org.example.utils.HibernateUtil;
+
 public class Main{
-    public static void main(String[] args) {
-        Game game = new Game();
-        game.gameloop();
+    public static void main(String[] args) throws Exception {
+        HibernateUtil hibernateUtil = new HibernateUtil();
+        Player player = new Player(4, "ant", "ant123", "securePass123", 3, "room1", new PlayerCell(new Coordinates(0, 0)));
+        hibernateUtil.create(player);
+
+        hibernateUtil.shutdown();
+        // Game game = new Game();
+        // game.gameloop();
     }
 }
--- src/main/java/org/example/utils/HibernateUtil.java
+++ src/main/java/org/example/utils/HibernateUtil.java
@@ -1,15 +1,19 @@
 package org.example.utils;
 
 import org.example.classes.Player;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.boot.MetadataSources;
 import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 public class HibernateUtil {
     private SessionFactory sessionFactory;
 
-    protected void setUp() {
+    public HibernateUtil() {
         final StandardServiceRegistry registry =
             new StandardServiceRegistryBuilder()
 				.build();
@@ -25,4 +29,17 @@ public class HibernateUtil {
         }
     }
 
+    public void shutdown() throws Exception {
+        if ( sessionFactory != null ) {
+            sessionFactory.close();
+        }
+    }
+
+    public void create(Object object) {
+        try (Session session = sessionFactory.openSession()) {
+            session.beginTransaction();
+            session.persist(object);
+            session.getTransaction().commit();
+        }
+    }
 }
