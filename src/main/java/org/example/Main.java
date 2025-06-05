1c1af010758f8da3745486ff64ee8591175237b0
Ant <Drakarta3@gmail.com>
2025-06-05 11:30:56 +0200

    ready for merge

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,21 +1,8 @@
 package org.example;
 
-import org.example.classes.Player;
-import org.example.classes.rooms.Coordinates;
-import org.example.classes.rooms.cells.PlayerCell;
-import org.example.utils.HibernateUtil;
-
 public class Main{
-    public static void main(String[] args) throws Exception {
-        HibernateUtil hibernateUtil = new HibernateUtil();
-        Player player = new Player(4, "ant", "ant123", "securePass123", 3, "room1", new PlayerCell(new Coordinates(0, 0)));
-        hibernateUtil.delete(player);
-        hibernateUtil.create(player);
-        Object test = hibernateUtil.read("FROM Player WHERE id = 4", Player.class);
-        System.out.println(test);
-
-        hibernateUtil.shutdown();
-        // Game game = new Game();
-        // game.gameloop();
+    public static void main(String[] args) {
+        Game game = new Game();
+        game.gameloop();
     }
 }
--- src/main/java/org/example/utils/HibernateUtil.java
+++ src/main/java/org/example/utils/HibernateUtil.java
@@ -68,3 +68,12 @@ public class HibernateUtil {
         }
     }
 }
+
+
+//         HibernateUtil hibernateUtil = new HibernateUtil();
+//         Player player = new Player(4, "ant", "ant123", "securePass123", 3, "room1", new PlayerCell(new Coordinates(0, 0)));
+//         hibernateUtil.delete(player);
+//         hibernateUtil.create(player);
+//         Object test = hibernateUtil.read("FROM Player WHERE id = 4", Player.class);
+//         System.out.println(test);
+//         hibernateUtil.shutdown();
