11acacfc564d09d7126e0e6be7bcd8b25d74bd9b
Ant <Drakarta3@gmail.com>
2025-06-05 11:19:25 +0200

    crud made

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -9,7 +9,10 @@ public class Main{
     public static void main(String[] args) throws Exception {
         HibernateUtil hibernateUtil = new HibernateUtil();
         Player player = new Player(4, "ant", "ant123", "securePass123", 3, "room1", new PlayerCell(new Coordinates(0, 0)));
+        hibernateUtil.delete(player);
         hibernateUtil.create(player);
+        Object test = hibernateUtil.read("FROM Player WHERE id = 4", Player.class);
+        System.out.println(test);
 
         hibernateUtil.shutdown();
         // Game game = new Game();
--- src/main/java/org/example/utils/HibernateUtil.java
+++ src/main/java/org/example/utils/HibernateUtil.java
@@ -6,9 +6,6 @@ import org.hibernate.SessionFactory;
 import org.hibernate.boot.MetadataSources;
 import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
 
 public class HibernateUtil {
     private SessionFactory sessionFactory;
@@ -39,6 +36,34 @@ public class HibernateUtil {
         try (Session session = sessionFactory.openSession()) {
             session.beginTransaction();
             session.persist(object);
+            session.getTransaction().commit();
+        }
+    }
+
+    public Object read(String query, Class<?> clazz) {
+        try (Session session = sessionFactory.openSession()) {
+            session.beginTransaction();
+            Object result = session.createQuery(query, clazz).getSingleResult();
+            session.getTransaction().commit();
+            return result;
+        } catch (Exception e) {
+            System.out.println("Error reading from database: " + e.getMessage());
+            return null;
+        }
+    }
+
+    public void update(Object object) {
+        try (Session session = sessionFactory.openSession()) {
+            session.beginTransaction();
+            session.merge(object);
+            session.getTransaction().commit();
+        }
+    }
+
+    public void delete(Object object) {
+        try (Session session = sessionFactory.openSession()) {
+            session.beginTransaction();
+            session.remove(object);
             session.getTransaction().commit();
         }
     }
