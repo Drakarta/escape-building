bb30173bef9aeae6486c091a366ab072302f0462
alexander <24094714@student.hhs.nl>
2025-06-13 10:41:45 +0200

    hinys :)

--- src/main/java/org/example/classes/questions/QuestionsList.java
+++ src/main/java/org/example/classes/questions/QuestionsList.java
@@ -36,8 +36,8 @@ public class QuestionsList {
         questionOrAnswer.add("1 maand");
         question = "Hoelang duren de sprints voor ons?";
         questiontype = "multipleChoiceQuestions";
-        hintList.add(new FunnyHint("Heb jij de peuterspeelzaal wel gehaald?"));
-        hintList.add(new HelpHint("Je kan op je vingers tellen hoeveel er zijn."));
+        hintList.add(new FunnyHint("We hebben net genoeg tijd gehad om fortnite te spelen"));
+        hintList.add(new HelpHint("3 sprints in 6 weken."));
         questionslist.add(new QuestionsForm(questionSort, questiontype, question, new ArrayList<>(questionOrAnswer), new ArrayList<>(hintList)));
         questionOrAnswer.clear();
         hintList.clear();
@@ -60,8 +60,8 @@ public class QuestionsList {
         questionOrAnswer.add("Done");
         question = "Wat zijn de onderdelen van een trello board?";
         questiontype = "multipleAnswersQuestion";
-        hintList.add(new FunnyHint("Heb jij de peuterspeelzaal wel gehaald?"));
-        hintList.add(new HelpHint("Je kan op je vingers tellen hoeveel er zijn."));
+        hintList.add(new FunnyHint("Er zijn meer dan 2 onderdelen."));
+        hintList.add(new HelpHint("Veel success man kon geen goede hint maken."));
         questionslist.add(new QuestionsForm(questionSort, questiontype, question, new ArrayList<>(questionOrAnswer), new ArrayList<>(hintList)));
         questionOrAnswer.clear();
         hintList.clear();
@@ -73,8 +73,8 @@ public class QuestionsList {
         questionOrAnswer.add("Hoeveel je hebt geslapen.");
         question = "Wat moet in de daily standup worden genoemd?";
         questiontype = "multipleChoiceQuestions";
-        hintList.add(new FunnyHint("Heb jij de peuterspeelzaal wel gehaald?"));
-        hintList.add(new HelpHint("Je kan op je vingers tellen hoeveel er zijn."));
+        hintList.add(new FunnyHint("Niemand boeit je prive leven loser."));
+        hintList.add(new HelpHint("Het moet relevant zijn voor iedereen."));
         questionslist.add(new QuestionsForm(questionSort, questiontype, question, new ArrayList<>(questionOrAnswer), new ArrayList<>(hintList)));
         questionOrAnswer.clear();
         hintList.clear();
@@ -85,8 +85,8 @@ public class QuestionsList {
         questionOrAnswer.add("Aan het einde van de week.");
         question = "Wanneer houd je een sprint review?";
         questiontype = "multipleChoiceQuestions";
-        hintList.add(new FunnyHint("Heb jij de peuterspeelzaal wel gehaald?"));
-        hintList.add(new HelpHint("Je kan op je vingers tellen hoeveel er zijn."));
+        hintList.add(new FunnyHint("Het is niet een retrospective."));
+        hintList.add(new HelpHint("Een review gaat over de vorige sprint."));
         questionslist.add(new QuestionsForm(questionSort, questiontype, question, new ArrayList<>(questionOrAnswer), new ArrayList<>(hintList)));
         questionOrAnswer.clear();
         hintList.clear();
@@ -97,8 +97,8 @@ public class QuestionsList {
         questionOrAnswer.add("Aan het einde van de week.");
         question = "Wanneer houd je een sprint retrospective?";
         questiontype = "multipleChoiceQuestions";
-        hintList.add(new FunnyHint("Heb jij de peuterspeelzaal wel gehaald?"));
-        hintList.add(new HelpHint("Je kan op je vingers tellen hoeveel er zijn."));
+        hintList.add(new FunnyHint("Het is niet een review."));
+        hintList.add(new HelpHint("Een retrospective gaat over de afgelopen sprint."));
         questionslist.add(new QuestionsForm(questionSort, questiontype, question, new ArrayList<>(questionOrAnswer), new ArrayList<>(hintList)));
         questionOrAnswer.clear();
         hintList.clear();
